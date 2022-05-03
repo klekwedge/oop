5f2d2da8276bae62897b1917605521d623cfe59c
klekkwedge <antonov.andrey.10101@gmail.com>
2022-05-03 20:29:10 +0300

    fix: remove unused code

--- 01. Objects, Prototypes, Inheritance/script.js
+++ 01. Objects, Prototypes, Inheritance/script.js
@@ -1,30 +1,4 @@
 "use strict";
-
-// const a = {
-//   text: "Hello",
-//   color: "red",
-//   bold: "true",
-//   show: function () {
-//     console.log(this.color);
-//   },
-
-// };
-
-// const b = {
-//   fontSize: "24px",
-//   __proto__: a,
-// };
-
-// const c = {
-//   fontFamily: "Roboto",
-//   __proto__: b,
-// };
-
-// console.log(c);
-// c.show();
-// console.log(c.hasOwnProperty('fontFamily'));
-// console.log(c.hasOwnProperty('bold'));
-
 const a = {
   text: "Hello",
   color: "red",
